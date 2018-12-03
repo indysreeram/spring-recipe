399866e532809457ba3a3defecb4391fecc3a5b7
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-12-03 07:35:29 -0500

    added query method

--- src/main/java/com/sreeram/springrecipe/controllers/IndexController.java
+++ src/main/java/com/sreeram/springrecipe/controllers/IndexController.java
@@ -1,18 +1,39 @@
 package com.sreeram.springrecipe.controllers;
 
+import com.sreeram.springrecipe.domain.Category;
+import com.sreeram.springrecipe.domain.UnitOfMeasure;
+import com.sreeram.springrecipe.repositories.CategoryRepository;
+import com.sreeram.springrecipe.repositories.UnitOfMeasureRepository;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
+
 @Controller
 public class IndexController {
+
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
 
     @Value("${spring.message}")
     private String message;
 
     @RequestMapping({"","/","index","index.html"})
     public String getIndexPage(Model model){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat :id "+categoryOptional.get().getId());
+        System.out.println("UOM :id "+categoryOptional.get().getId());
+
         System.out.println("I am in the index controller!!!");
          return "index";
     }
--- src/main/java/com/sreeram/springrecipe/repositories/CategoryRepository.java
+++ src/main/java/com/sreeram/springrecipe/repositories/CategoryRepository.java
@@ -3,5 +3,10 @@ package com.sreeram.springrecipe.repositories;
 import com.sreeram.springrecipe.domain.Category;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends CrudRepository<Category,Long> {
+
+
+    Optional<Category> findByDescription(String description);
 }
