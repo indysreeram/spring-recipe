290d891efb1ca527617e34f38607f6ce62c1bfb0
Sreeram Srinivasaragavan <indysreeram@gmail.com>
2018-12-24 06:06:02 -0500

    added a mockito sample test

---
+++ src/test/java/com/sreeram/springrecipe/services/RecipeServiceImplTest.java
@@ -0,0 +1,47 @@
+package com.sreeram.springrecipe.services;
+
+import com.sreeram.springrecipe.domain.Recipe;
+import com.sreeram.springrecipe.repositories.RecipeRepository;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+public class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @Before
+    public void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
+
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    public void getRecipes() {
+
+        Recipe recipe = new Recipe();
+
+        HashSet<Recipe> recipeData = new HashSet<>();
+        recipeData.add(recipe);
+
+
+        when(recipeService.getRecipes()).thenReturn(recipeData);
+
+        Set<Recipe>  recipes = recipeService.getRecipes();
+
+        assertEquals(recipes.size(),1);
+
+        verify(recipeRepository,times(1)).findAll();
+    }
+}
